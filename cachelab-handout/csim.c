f0cab9389a814ae18a12e521b5f1ede2deda20b5
Sungchan Yi <calofmijuck@snu.ac.kr>
2018-12-05 14:48:04 +0900

    Update 1

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,11 +1,24 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 #include "cachelab.h"
 
-int main()
-{
+typedef unsigned long long memAddress; // 64-bit data to hold memory addresses
+
+typedef struct {
+    int s; // bits needed to represent S = 2 ** s
+    int b; // B = 2 ** b
+    int E; // Cache lines per set
+    int S; // Number of sets
+    int B; // Cache line block size in bytes
+    int hits;
+    int misses;
+    int evicts;
+} cache_param;
+
+int main(int argc, char** argv) {
+    cache_param param;
+    memset(&param, 0, sizeof(param));
     printSummary(0, 0, 0);
     return 0;
 }
