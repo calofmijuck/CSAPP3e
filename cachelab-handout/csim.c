715cf15d3bb816440d616f3b8b5af1aa19620b6f
Sungchan Yi <calofmijuck@snu.ac.kr>
2018-12-05 15:01:03 +0900

    Update 2 - getopt

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,6 +1,7 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 #include "cachelab.h"
 
 typedef unsigned long long memAddress; // 64-bit data to hold memory addresses
@@ -16,9 +17,38 @@ typedef struct {
     int evicts;
 } cache_param;
 
+int verbosity; // -v flag
+
 int main(int argc, char** argv) {
     cache_param param;
-    memset(&param, 0, sizeof(param));
-    printSummary(0, 0, 0);
+    malloc(sizeof(param));
+    // printSummary(0, 0, 0);
+
+    FILE *read_trace;
+    char op;
+    memAddress addr;
+    int size;
+    char *trace_file, c;
+
+    while((c = getopt(argc, argv, "s:E:b:t:vh")) != -1) {
+        switch (c) {
+            case 's':
+                param.s = atoi(optarg);
+                break;
+            case 'E':
+                param.E = atoi(optarg);
+                break;
+            case 'b':
+                param.b = atoi(optarg);
+            case 't':
+                trace_file = optarg;
+            case 'v':
+                verbosity = 1;
+                break;
+            // case 'h':
+            default:
+                exit(1);
+        }
+    }
     return 0;
 }
