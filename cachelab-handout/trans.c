860637c6d357f08bb22510c0dc65177099a99855
Sungchan Yi <calofmijuck@snu.ac.kr>
2018-12-10 12:55:57 +0900

    Load first

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -47,12 +47,15 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
 
     // When using the same method as in N = 32, the driver.py gives 0 score... (checked for size = 8, 16)
     // Suggestion of something similar to loop unrolling?
+    // gives 1635 misses (4.2/8)
     else if(N == 64) {
         size = 4; // choose size;
         for(r = 0; r < N; r += size) {
             for(c = 0; c < M; c += size) {
                 a = A[r][c]; // load (r, c)
                 s = A[r + 1][c]; // load (r + 1, c)
+                d = A[r + 2][c]; // load (r + 2, c)
+                f = A[r + 2][c + 1]; // load (r + 2, c + 1)
                 g = A[r + 2][c + 2]; // load (r + 2, c + 2)
 
                 B[c + 3][r] = A[r][c + 3];
@@ -63,8 +66,7 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
                 B[c + 2][r + 1] = A[r + 1][c + 2];
                 B[c + 2][r + 2] = g;
 
-                d = A[r + 2][c]; // load (r + 2, c)
-                f = A[r + 2][c + 1]; // load (r + 2, c + 1)
+
                 g = A[r + 1][c + 1]; // update
 
                 B[c + 1][r] = A[r][c + 1];
